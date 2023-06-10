bb5ccf75927a399e7df8b3de77e5aa85f50f8546
Teckwar160 <teckwar160@gmail.com>
2023-06-10 01:30:53 -0600

    Se avanzo en la practica 10

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,21 +5,62 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public Transform jugador;
-    public bool perseguir = false;
-    UnityEngine.AI.NavMeshAgent enemigo;
     public Animator controlEnemigo;
+    bool flagPerseguir = false;
+    bool flagGolpear = false;
+    bool flagMorir = false;
+    UnityEngine.AI.NavMeshAgent enemigo;
+    float radio = 1.0f;
     
     void Start() {
         enemigo = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
-    
-    void Update(){
-        if(perseguir){
-            enemigo.destination = jugador.position;
-            controlEnemigo.SetBool("Perseguir",perseguir);
+    void OnTriggerEnter(Collider collider){
+        if (collider.gameObject.name == "Jugador"){
+            enemigo.isStopped = false;
+            flagPerseguir = true;
+            flagGolpear = false;
+
         }
-        controlEnemigo.SetBool("Perseguir",perseguir);
+    }
+
+    void OnTriggerExit(Collider collider){
+        if (collider.gameObject.name == "Jugador"){
+            enemigo.isStopped = true;
+            flagPerseguir = false;
+            flagGolpear = false;
+
+        }
+    }
+    
+
+    void golpear(){
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radio);
+        foreach (Collider collider in colliders){
+            if (collider.gameObject.name == "Jugador"){
+                enemigo.isStopped = true;
+                flagGolpear = true;
+                flagPerseguir = false;
+
+            } else if (collider.CompareTag("Bala")){
+                enemigo.isStopped = true;
+                flagGolpear = false;
+                flagPerseguir = false;
+                flagMorir = true;
+            }
+        }
+    }
+
+    void Update(){
+        golpear();
+
+        if(flagPerseguir){
+            enemigo.destination = jugador.position;
+        }
+        controlEnemigo.SetBool("Perseguir",flagPerseguir);
+        controlEnemigo.SetBool("Golpear",flagGolpear);
+        controlEnemigo.SetBool("Morir",flagMorir);
             
     }
 }
